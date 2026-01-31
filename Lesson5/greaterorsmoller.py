5c10fbeebc764bce5a658db44abdc5ae456dbd07
encokwanje-a11y <encokwanje@gmail.com>
2026-01-31 17:08:15 +0300

    commit

---
+++ Lesson5/greaterorsmoller.py
@@ -0,0 +1,10 @@
+i = int(input("enter a number : "))
+if (i < 15):
+    print ("i is smaller than 15")
+    print ("im in if Block")
+else:
+    print ("i is greater than 15")
+    print ("im in else Block")
+print ("im not in if end not in else block")
+
+        
