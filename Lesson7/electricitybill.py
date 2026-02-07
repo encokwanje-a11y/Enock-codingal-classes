922bd27416a33ee545584d4d7363abdbe99c9f9f
encokwanje-a11y <encokwanje@gmail.com>
2026-02-07 17:06:50 +0300

    Commit

---
+++ Lesson7/electricitybill.py
@@ -0,0 +1,30 @@
+#Take input of number of units consumed from the user
+units = int(input(" Please enter Number of units  you consumed :"))
+
+# check condition of units consumed 
+# Then calculate amount and surcharge accordingly
+#surchange is the tax value
+
+#  check for units less than 50
+if(units <= 50):
+    amount = units * 2.60
+    surcharge = 25
+
+
+elif(units <= 100):
+     amount = 130 + ((units - 50) + 3.25)
+     surcharge = 35
+
+
+elif(units <= 200):
+  amount = 130 + 162.50 + ((units - 100) * 5.26) 
+  surcharge = 45
+
+
+else:
+    amount = 130 + 162.50 +526 + ((units - 200) * 8.45) 
+    surcharge = 75
+
+
+total = amount + surcharge
+print("\nElectricity Bill = %.2f" %total)
